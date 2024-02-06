64bbdc6bff5165f77e31414486757303ed547890
LokeshwarMERN <lokeshwar.prasad.cse@gmail.com>
2024-02-06 23:09:08 +0530

    fixed typo

--- backend/src/app.js
+++ backend/src/app.js
@@ -5,13 +5,13 @@ import MessageRoutes from "./routes/message.routes.js"
 
 // config cors (cross origin resources service)
 app.use(cors({
-    origin: ["https://lokeshwar-creatives.netlify.app/", "https://lokeshwar-creatives.onrender.com/"],
+    origin: ["https://lokeshwar-creatives.netlify.app", "https://lokeshwar-creatives.onrender.com"],
     credentials: true
 }))
 
 // accept data from frontend
 app.use(express.json({ limit: "16kb" }))
-app.use(express.urlencoded({extended: true, limit: "16kb"}))
+app.use(express.urlencoded({ extended: true, limit: "16kb" }))
 
 // api endpoint
 app.use("/api/v1/users", MessageRoutes)
@@ -19,9 +19,4 @@ app.use("/api/v1/users", MessageRoutes)
 
 
 
-
-
-
-
-
-export {app}
+export { app }
--- backend/src/controllers/messages.controllers.js
+++ backend/src/controllers/messages.controllers.js
@@ -1,5 +1,5 @@
 import { messageModel } from "../models/message.models.js"
-import { asyncHandler } from "../utils/AsyncHandler.js"
+import { asyncHandler } from "../utils/asyncHandler.js"
 import { ApiError } from "../utils/ApiError.js"
 import { ApiResponse } from "../utils/ApiResponse.js"
 
