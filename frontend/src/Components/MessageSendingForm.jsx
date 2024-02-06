437cf108c3b241d4166a6ae0ac58e2625b530aac
LokeshwarMERN <lokeshwar.prasad.cse@gmail.com>
2024-02-06 18:38:29 +0530

    Implemented form validation for contact

--- frontend/src/Components/MessageSendingForm.jsx
+++ frontend/src/Components/MessageSendingForm.jsx
@@ -1,10 +1,39 @@
+import { useState } from "react";
 import "../CSS/MessageSendingForm.css";
 import { ToastContainer, toast } from "react-toastify";
+import { host } from "../Utils/APIHost";
+import axios from "axios"
 
 const MessageSendingForm = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
+
   const submitMessage = (e) => {
     e.preventDefault();
+    if (!name.length > 5) {
+      toast.error("Name must more than 5 characters!", { autoClose: 3000 });
+      return;
+    }
+    if (!email || !message) {
+      toast.error("Fill all fields!", { autoClose: 3000 });
+      return;
+    }
+    console.log(`name : ${name} | email: ${email} | message : ${message}`)
+
+    // post request to admin
+    // const config = {
+    //   headers: {
+    //     "Accept" : "application/json"
+    //   }
+    // }
+    // const postData = { name, email, message };
+    // const { data } = axios.post(`${host}/endpoint`, postData, config)
+
     toast.success("Successfully Sent", { autoClose: 3000 });
+    setName("");
+    setEmail("");
+    setMessage("");
   };
 
   return (
@@ -15,22 +44,39 @@ const MessageSendingForm = () => {
             <ion-icon
               name="chatbubble-ellipses-outline"
               style={{ position: "relative", top: "2px", paddingRight: "4px" }}
-              ></ion-icon>
-              Message
-              Me
+            ></ion-icon>
+            Message Me
           </h2>
           <form>
             <div className="user-box">
-              <input type="text" name="" required="" placeholder="Your Name" />
-              {/* <label className="text-xl" >Your Name</label> */}
+              <input
+                onChange={(e) => {
+                  setName(e.target.value);
+                }}
+                type="text"
+                value={name}
+                placeholder="Your Name"
+                />
             </div>
             <div className="user-box">
-              <input type="text" name="" required="" placeholder="Your Email" />
-              {/* <label className="text-xl" >Email</label> */}
+              <input
+                onChange={(e) => {
+                  setEmail(e.target.value);
+                }}
+                type="email"
+                value={email}
+                placeholder="Your Email"
+                />
             </div>
             <div className="user-box">
-              <input type="text" name="" required="" placeholder="Message" />
-              {/* <label className="text-xl" >Message</label> */}
+              <textarea
+                onChange={(e) => {
+                  setMessage(e.target.value);
+                }}
+                id="message_box"
+                placeholder="Message"
+                value={message}
+              ></textarea>
             </div>
             <button onClick={submitMessage}>
               <span></span>
