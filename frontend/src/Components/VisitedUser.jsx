5216f8e3c48e1c20f0b82d0c00a30d18c3a8cee8
LokeshwarMERN <lokeshwar.prasad.cse@gmail.com>
2024-03-10 21:09:17 +0530

    spelling corrected

--- frontend/src/Components/VisitedUser.jsx
+++ frontend/src/Components/VisitedUser.jsx
@@ -87,7 +87,7 @@ const VisitedUser = () => {
   return (
     <>
       <div className="visited_user_container text-gray-400 flex justify-center items-center text-md font-overpass mt-10">
-        <p className="">VISITED USER :&nbsp;</p>
+        <p className="">VISITED USERS :&nbsp;</p>
         <b>{visitedUsers}</b>
       </div>
     </>
