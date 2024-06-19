3f22c29dbea8358c263cc710a7f55215115ba250
LokeshwarMERN <lokeshwar.prasad.cse@gmail.com>
2024-06-19 21:18:02 +0530

    update skills boxes

--- frontend/src/Components/TechnicalSkills.jsx
+++ frontend/src/Components/TechnicalSkills.jsx
@@ -15,8 +15,8 @@ const TechnicalSkills = () => {
         <div className="underline_design w-32 md:w-44 h-[2px] bg-slate-500"></div>
 
         <div className="skill_programming_lang skills_box_class ">
-          <h3 className="py-2 text-lg">Programming Languages</h3>
-          <div className="programming_language_container flex justify-center flex-wrap gap-6 items-center ">
+          <h3 className="pb-4 text-lg">Programming Languages</h3>
+          <div className="programming_language_container flex justify-center flex-wrap items-center ">
             {programmingLangArray.map((lang, index) => (
               <React.Fragment key={index}>
                 <SkillsBox data={lang} />
@@ -27,8 +27,8 @@ const TechnicalSkills = () => {
           <hr className="opacity-20 w-[60%]" />
         </div>
         <div className="skill_programming_lang skills_box_class ">
-          <h3 className="py-2 text-lg">Technologies</h3>
-          <div className="programming_language_container flex justify-center flex-wrap gap-6 items-center ">
+          <h3 className="pb-4 text-lg">Technologies</h3>
+          <div className="programming_language_container flex justify-center flex-wrap items-center ">
             {webTechnologiesArray.map((technology, index) => (
               <React.Fragment key={index}>
                 <SkillsBox data={technology} />
@@ -39,8 +39,8 @@ const TechnicalSkills = () => {
         <br />
         <hr className="opacity-20 w-[60%]" />
         <div className="skill_programming_lang skills_box_class ">
-          <h3 className="py-2 text-lg">Databases</h3>
-          <div className="programming_language_container flex justify-center flex-wrap gap-6 items-center ">
+          <h3 className="pb-4 text-lg">Databases</h3>
+          <div className="programming_language_container flex justify-center flex-wrap items-center ">
             {databaseArray.map((database, index) => (
               <React.Fragment key={index}>
                 <SkillsBox data={database} />
@@ -51,8 +51,8 @@ const TechnicalSkills = () => {
         <br />
         <hr className="opacity-20 w-[60%]" />
         <div className="skill_programming_lang skills_box_class ">
-          <h3 className="py-2 text-lg">Version Control System</h3>
-          <div className="programming_language_container flex justify-center flex-wrap gap-6 items-center ">
+          <h3 className="pb-4 text-lg">Version Control System</h3>
+          <div className="programming_language_container flex justify-center flex-wrap items-center ">
             {versionControlArray.map((vcs, index) => (
               <React.Fragment key={index}>
                 <SkillsBox data={vcs} />
--- frontend/src/Group/SkillsBox.jsx
+++ frontend/src/Group/SkillsBox.jsx
@@ -1,10 +1,13 @@
 const SkillsBox = ({ data }) => {
   return (
     <>
-      <div className="skill_box flex flex-col justify-center items-center font-overpass bg-slate-800 w-48 pb-3 shadow-sm shadow-blue-400 hover:scale-105 custom-transition cursor-pointer">
-        <div className="image_skill flex justify-center items-center py-4">
+      <div
+        className="skill_box flex flex-col justify-center items-center font-overpass 
+      w-40 pb-3 hover:scale-105 custom-transition cursor-pointer rounded-xl py-2"
+      >
+        <div className="image_skill flex justify-center items-center pb-2">
           <img
-            className="h-28 object-cover w-28 rounded-full"
+            className="h-28 object-cover w-28 rounded-2xl opacity-80 shadow-sm shadow-gray-600"
             src={data.src}
             alt={data.title}
             loading="lazy"
